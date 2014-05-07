311b21fe8548626d997826e57a5df748749bfb53
Kosma Moczek <kosma@cloudyourcar.com>
2014-05-07 16:11:17 +0200

    fuzzer.py: add first fuzzing

--- tests/fuzzer.py
+++ tests/fuzzer.py
@@ -4,18 +4,38 @@
 from pyflashsim import FlashSim
 from pyringfs import RingFSFlashPartition, RingFS
 
-def op_sector_erase(address):
-    sim.sector_erase(address)
-    return 0
 
-def op_program(address, data):
-    sim.program(address, data)
-    return len(data)
+class FuzzRun(object):
 
-def op_read(address, size):
-    return sim.read(address, size)
+    def __init__(self, name, version, object_size, sector_size, total_sectors, sector_offset, sector_count):
 
-sim = FlashSim('tests/fuzzer.sim', 16*1024, 1024)
-flash = RingFSFlashPartition(1024, 0, 16,
-        op_sector_erase, op_program, op_read)
-fs = RingFS(flash, 0x42, 16)
+        sim = FlashSim(name, total_sectors*sector_size, sector_size)
+
+        def op_sector_erase(address):
+            sim.sector_erase(address)
+            return 0
+
+        def op_program(address, data):
+            sim.program(address, data)
+            return len(data)
+
+        def op_read(address, size):
+            return sim.read(address, size)
+
+        self.version = version
+        self.object_size = object_size
+
+        self.flash = RingFSFlashPartition(sector_size, sector_offset, sector_count,
+                op_sector_erase, op_program, op_read)
+        self.fs = RingFS(self.flash, version, object_size)
+
+    def run(self):
+
+        self.fs.format()
+
+        for i in xrange(1000):
+            self.fs.append('x'*self.object_size)
+
+
+f = FuzzRun('tests/fuzzer.sim', 0x42, 16, 1024, 16, 0, 16)
+f.run()
