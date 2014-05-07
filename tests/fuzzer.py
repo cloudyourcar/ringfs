1da19559d9b6d0c09b8f3051a5c5f21a7b61b445
Kosma Moczek <kosma@cloudyourcar.com>
2014-05-07 17:04:12 +0200

    much better fuzzer.

--- tests/fuzzer.py
+++ tests/fuzzer.py
@@ -7,9 +7,18 @@ from pyflashsim import FlashSim
 from pyringfs import RingFSFlashPartition, RingFS
 
 
+def compare(a, b):
+    if a != b:
+        print ">>> %s != %s" % (a, b)
+    return a == b
+
+
 class FuzzRun(object):
 
     def __init__(self, name, version, object_size, sector_size, total_sectors, sector_offset, sector_count):
+
+        print "FuzzRun[%s]: v=%08x os=%d ss=%d ts=%d so=%d sc=%d" % (name, version, object_size, sector_size,
+                total_sectors, sector_offset, sector_count)
 
         sim = FlashSim(name, total_sectors*sector_size, sector_size)
 
@@ -34,6 +43,7 @@ class FuzzRun(object):
     def run(self):
 
         self.fs.format()
+        self.fs.dump()
 
         def do_append():
             self.fs.append('x'*self.object_size)
@@ -54,12 +64,26 @@ class FuzzRun(object):
 
             # consistency check
             newfs = RingFS(self.flash, self.version, self.object_size)
-            assert newfs.scan() == 0
-            assert newfs.ringfs.read.sector == self.fs.ringfs.read.sector
-            assert newfs.ringfs.read.slot == self.fs.ringfs.read.slot
-            assert newfs.ringfs.write.sector == self.fs.ringfs.write.sector
-            assert newfs.ringfs.write.slot == self.fs.ringfs.write.slot
+            try:
+                assert newfs.scan() == 0
+                assert compare(newfs.ringfs.read.sector, self.fs.ringfs.read.sector)
+                assert compare(newfs.ringfs.read.slot, self.fs.ringfs.read.slot)
+                assert compare(newfs.ringfs.write.sector, self.fs.ringfs.write.sector)
+                assert compare(newfs.ringfs.write.slot, self.fs.ringfs.write.slot)
+            except AssertionError:
+                print "self.fs:"
+                self.fs.dump()
+                print "newfs:"
+                newfs.dump()
+                raise
 
 
-f = FuzzRun('tests/fuzzer.sim', 0x42, 16, 1024, 16, 0, 16)
+sector_size = random.randint(16, 256)
+total_sectors = random.randint(2, 8)
+sector_offset = random.randint(0, total_sectors-2)
+sector_count = random.randint(2, total_sectors-sector_offset)
+version = random.randint(0, 0xffffffff)
+object_size = random.randint(1, sector_size-8)
+
+f = FuzzRun('tests/fuzzer.sim', version, object_size, sector_size, total_sectors, sector_offset, sector_count)
 f.run()
